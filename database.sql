897572de2299cea198bec2799503faeb88f095f0
pah.dev <jeanpaulb79@gmail.com>
2022-03-01 15:19:41 +0100

    [ADD] sql file

---
+++ database.sql
@@ -0,0 +1,25 @@
+INSERT INTO wallets (hash, created_at, updated_at, deleted_at, alias, balance) VALUES ('85a14d53194cae90164df66d56482bdb811af19777ee0b3e105d831eb177f8f9',
+$(NOW),$(NOW),NULL, '214523BTC', 95.56);
+INSERT INTO wallets (hash, created_at, updated_at, deleted_at, alias, balance) VALUES ('b63e1194205ede6a0a4ef49a4e3cf7d24aff85002304249aa5cc41430ec35bdc',
+$(NOW),$(NOW),NULL, '202523BTC', 195.36);
+INSERT INTO wallets (hash, created_at, updated_at, deleted_at, alias, balance) VALUES ('192617de81b47b1865a162c99a5f2f9c0e811fd9436f3f90c87456f1ffa7f9b9',
+$(NOW),$(NOW),NULL, '574497BTC', 1146.83);
+INSERT INTO wallets (hash, created_at, updated_at, deleted_at, alias, balance) VALUES ('1b8defc511a68b1998d8264ae36a33ab5260c9f9fa9fb0d52ac4abf1fca4a6ef',
+$(NOW),$(NOW),NULL, '163800BTC', 72.97);
+INSERT INTO wallets (hash, created_at, updated_at, deleted_at, alias, balance) VALUES ('22716563322dbfe4132323ac98a6d38da1671befd92bc5d7ef8014b069e76dfb',
+$(NOW),$(NOW),NULL, '250616BTC', 7684.44);
+INSERT INTO wallets (hash, created_at, updated_at, deleted_at, alias, balance) VALUES ('4f8eaed9f764fd34abe34f02148924cf478bb7c2efd96a584f11cd4031dc85b1',
+$(NOW),$(NOW),NULL, '468372BTC', 4217.77);
+INSERT INTO wallets (hash, created_at, updated_at, deleted_at, alias, balance) VALUES ('131493f8421d06bca9e4ef9d1ed3dc24a5f7ac42647fd5266906cc55efbbceb7',
+$(NOW),$(NOW),NULL, '401996BTC', 1069.99);
+INSERT INTO wallets (hash, created_at, updated_at, deleted_at, alias, balance) VALUES ('7bd63d6bd9d5873e5ad1a1fb94e133de1876f832ce10c7caaafa095af158c097',
+$(NOW),$(NOW),NULL, '518978BTC', 231.18);
+INSERT INTO wallets (hash, created_at, updated_at, deleted_at, alias, balance) VALUES ('702cbbb6566a0613f74c884ba9cea08efb30e0410a05797439d7fb7ae91e25b2',
+$(NOW),$(NOW),NULL, '422722BTC', 49188.77);
+INSERT INTO wallets (hash, created_at, updated_at, deleted_at, alias, balance) VALUES ('a90dbd2c360ea90e040e0aef1c1938d4c9b770f55acaf85f37ffee08b836fcd3',
+$(NOW),$(NOW),NULL, '507945BTC', 671.73);
+INSERT INTO wallets (hash, created_at, updated_at, deleted_at, alias, balance) VALUES ('a5b0365c94463efa4302c8e9a02228c954ec11a0da199639ff40bb7b40969e4f',
+$(NOW),$(NOW),NULL, '140083BTC', 62.40);
+
+INSERT INTO auths (created_at, updated_at, deleted_at, username, password) 
+VALUES ($(NOW), $(NOW), NULL, 'admin', 'admin');
